0cb301b2a313c6efed9a319fad5663971f100420
WIN-UJOGROUEKSL\AVEKS_UZ <yusupovdoston93@gmail.com>
2021-11-09 04:53:39 +0500

    add hero &  info sections

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
-import Navbar from './components/Navbar';
+import Home from './pages';
 import './App.css';
 
 function App() {
-  return (
-    <Router>
-      <Navbar />
-    </Router>
-  );
+	return (
+		<Router>
+			<Home />
+		</Router>
+	);
 }
 
 export default App;
--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,18 +1,47 @@
 import React from 'react';
-import { Nav, NavbarContainer, NavLogo } from './NavbarElements';
+import {
+	Nav,
+	NavbarContainer,
+	NavLogo,
+	MobileIcon,
+	NavMenu,
+	NavItem,
+	NavLinks,
+	NavBtn,
+	NavBtnLink,
+} from './NavbarElements';
+import { FaBars } from 'react-icons/fa';
 
-const Navbar = () => {
+const Navbar = ({ toggle }) => {
 	return (
 		<>
 			<Nav>
 				<NavbarContainer>
-					<NavLogo to='/'>
-						dolla
-					</NavLogo>
+					<NavLogo to='/'>dolla</NavLogo>
+					<MobileIcon onClick={toggle}>
+						<FaBars />
+					</MobileIcon>
+					<NavMenu>
+						<NavItem>
+							<NavLinks to='about'>About</NavLinks>
+						</NavItem>
+						<NavItem>
+							<NavLinks to='discover'>Discover</NavLinks>
+						</NavItem>
+						<NavItem>
+							<NavLinks to='services'>Services</NavLinks>
+						</NavItem>
+						<NavItem>
+							<NavLinks to='singup'>Sing Up</NavLinks>
+						</NavItem>
+					</NavMenu>
+					<NavBtn>
+						<NavBtnLink to='/signin'>Sign In</NavBtnLink>
+					</NavBtn>
 				</NavbarContainer>
 			</Nav>
 		</>
-	)
+	);
 };
 
 export default Navbar;
