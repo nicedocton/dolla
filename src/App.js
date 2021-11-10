aea9dd15d1c03ae9b96411bf6f29689620803573
WIN-UJOGROUEKSL\AVEKS_UZ <yusupovdoston93@gmail.com>
2021-11-10 22:47:45 +0500

    fix bug on github pages333

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import './App.css';
 
 function App() {
 	return (
-		<Router>
+		<Router basename='/dolla'>
 			{/* <Switch> */}
 			<Route path='/' component={Home} exact />
 			{/* <Route path='/signin' component={SinginPage} exact /> */}
