9ef27f9125d5cf223c69be02b1be1a8f977c0c7e
WIN-UJOGROUEKSL\AVEKS_UZ <yusupovdoston93@gmail.com>
2021-11-10 22:18:23 +0500

    fix errors in github pages

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Home from './pages';
-import SinginPage from './pages/singin';
+//import SinginPage from './pages/singin';
 import './App.css';
 
 function App() {
